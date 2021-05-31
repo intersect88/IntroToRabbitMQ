f8ce4e14888a14723dae29f0c4ec437299eefc05
intersect88 <intersect@anche.no>
2021-05-31 22:03:12 +0200

    Extract method to publish messages and create simple method to fill an array of messages

--- IntroToRabbitMQ/Sender/Sender.cs
+++ IntroToRabbitMQ/Sender/Sender.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Linq;
 using RabbitMQ.Client;
 using System.Text;
+using System.Threading;
+using Polly;
+using RabbitMQ.Client.Exceptions;
 
 namespace Sender
 {
@@ -8,28 +12,61 @@ namespace Sender
     {
         public static void Main()
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            // Unhandled Exception: RabbitMQ.Client.Exceptions.BrokerUnreachableException: None of the specified endpoints were reachable
+            var retryPolicy = Policy.Handle<AlreadyClosedException>()
+                .WaitAndRetry(5,
+                    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    (exception, timeSpan, retryCount, context) =>
+                    {
+                        Console.WriteLine($"This is the {retryCount} try");
+                    }
+                    // .Retry(3, onRetry: (exception, i) => 
+                );
+
             using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "QueueDemo",
-                                     durable: false,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "QueueDemo",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+                    var messages = new string [100];
 
-                string message = "Demo Message";
-                var body = Encoding.UTF8.GetBytes(message);
+                    FillMessages(messages);
 
-                channel.BasicPublish(exchange: "",
-                                     routingKey: "QueueDemo",
-                                     basicProperties: null,
-                                     body: body);
-                Console.WriteLine("Sent {0}", message);
+                    foreach (var message in messages)
+                    {
+                        retryPolicy.Execute(() =>
+                            PublishMessage(message, channel)
+                        );
+                        Thread.Sleep(2000);
+                    }
+                }
             }
 
             Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
+        }
+
+        private static void FillMessages(string[] arrayToFill)
+        {
+            for (var j = 0; j < arrayToFill.Length; j++)
+            {
+                arrayToFill[j] = $"Message {j}";
+            }
+        }
+
+        private static void PublishMessage(string message, IModel channel)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "",
+                routingKey: "QueueDemo",
+                basicProperties: null,
+                body: body);
+            Console.WriteLine($"Sent {message}");
         }
     }
 }
